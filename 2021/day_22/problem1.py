747625fc4ec0a040e6ea06dd679170ad05aa58a1
David Holec <david.holec@unileoben.ac.at>
2021-12-22 21:03:56 +0100

    day 22, problem 1

--- 2021/day_22/problem1.py
+++ 2021/day_22/problem1.py
@@ -1,132 +1,21 @@
 from itertools import product
 
-def overlap(r1, r2):
-    return (max(r1[0], r2[0]) <= min(r1[1], r2[1])) and \
-        (max(r1[2], r2[2]) <= min(r1[3], r2[3])) and \
-        (max(r1[4], r2[4]) <= min(r1[5], r2[5]))
-    # return ((r1[1]>=r2[0]) or (r1[0]<=r2[1])) and \
-    #     ((r1[3]>=r2[2]) or (r1[2]<=r2[3])) and \
-    #     ((r1[5]>=r2[4]) or (r1[4]<=r2[5]))
-        
-def contained(r1, r2):
-    return ((r1[0]>=r2[0]) and (r1[1]<=r2[1]) and\
-        (r1[2]>=r2[2]) and (r1[3]<=r2[3]) and\
-        (r1[4]>=r2[4]) and (r1[5]<=r2[5]))
-    # or\
-    #     ((r1[0]<=r2[0]) and (r1[1]>=r2[1]) and \
-    #     (r1[2]<=r2[2]) and (r1[3]>=r2[3]) and \
-    #     (r1[4]<=r2[4]) and (r1[5]>=r2[5]))        
+cubes = set()
 
-def check_overlap(regions):
-    for i1 in range(len(regions)-1):
-        for i2 in range(i1+1, len(regions)):
-            if overlap(regions[i1], regions[i2]):
-                    return True, i1, i2
-    return False, None, None
-
-def chop_on(i1, i2, regions):
-    r1 = regions[i1]
-    r2 = regions[i2]
-    if contained(r1, r2):
-        new_regions = [r2]
-    elif contained(r2, r1):
-        new_regions = [r1]
-    else:
-        new_regions = []
-        shift_min = [0,0,1]
-        shift_max = [-1,0,0]
-        x = sorted([r1[0], r1[1], r2[0], r2[1]])
-        y = sorted([r1[2], r1[3], r2[2], r2[3]])
-        z = sorted([r1[4], r1[5], r2[4], r2[5]])
-        # print(x, y, z)
-        if (len(x)<=2) and (len(y)<=2) and (len(z)<=2):
-            new_regions = [r1]
-        else:
-            for ix, iy, iz in product(range(len(x)-1), range(len(y)-1), range(len(z)-1)):
-                r = [x[ix]+shift_min[ix], x[ix+1]+shift_max[ix],
-                    y[iy]+shift_min[iy], y[iy+1]+shift_max[iy],
-                    z[iz]+shift_min[iz], z[iz+1]+shift_max[iz]]
-                if contained(r, r1) or contained(r, r2):
-                    # print('*', r, '*', r1, contained(r, r1), ';', r2, contained(r, r2))
-                    # print('*', contained(r, r1), contained(r, r2))
-                    new_regions.append(r)
-    # print(len(regions))            
-    regions = regions[:i2]+regions[i2+1:]
-    regions = regions[:i1]+regions[i1+1:]
-    # print(len(regions), len(new_regions))
-    # print(r1, r2)
-    # print(new_regions)
-    regions += new_regions
-    return regions
-
-def chop_off(i1, off, regions):
-    new_regions = []
-    r1 = regions[i1]
-    r2 = off
-    if not contained(r1, off):
-        shift_min = [0,0,1]
-        shift_max = [-1,0,0]
-        x = sorted([r1[0], r1[1], r2[0], r2[1]])
-        y = sorted([r1[2], r1[3], r2[2], r2[3]])
-        z = sorted([r1[4], r1[5], r2[4], r2[5]])
-        # print(x, y, z)
-        if (len(x)<=2) and (len(y)<=2) and (len(z)<=2):
-            new_regions = []
-        else:
-            for ix, iy, iz in product(range(len(x)-1), range(len(y)-1), range(len(z)-1)):
-                r = [x[ix]+shift_min[ix], x[ix+1]+shift_max[ix],
-                    y[iy]+shift_min[iy], y[iy+1]+shift_max[iy],
-                    z[iz]+shift_min[iz], z[iz+1]+shift_max[iz]]
-                # print(r)
-                if contained(r, r1) and not contained(r, r2):
-                    # print('*', r, '*', r1, contained(r, r1), ';', r2, contained(r, r2))
-                    # print('*', contained(r, r1), contained(r, r2))
-                    # print(r)
-                    new_regions.append(r)
-    # print(len(regions))            
-    regions = regions[:i1]+regions[i1+1:]
-    # print(len(regions), len(new_regions))
-    # print(r1, r2)
-    # print(new_regions)
-    regions += new_regions
-    return regions
-    
-def process_line(line, regions):
-    command, coords = line.split(' ')
-    coords = [int(l.split('=')[-1]) for x in coords.split(',') for l in x.split('..')]
-    if command == 'on':
-        regions.append(coords)
-        # print(regions)
-        overlap, i1, i2 = check_overlap(regions)
-        # print(overlap)
-        while overlap:
-            # print(regions)
-            print(len(regions), i1, i2, regions[i1], regions[i2])
-            regions = chop_on(i1, i2, regions)
-            overlap, i1, i2 = check_overlap(regions)
-    elif command == 'off':
-        # print(command, coords)
-        overlap, i1, _ = check_overlap(regions+[coords])
-        while overlap:            
-            print(overlap, i1, regions[i1])
-            regions = chop_off(i1, coords, regions)            
-            overlap, i1, _ = check_overlap(regions+[coords])
-            # print(len(regions))
-    # print(len(regions))
-    return regions
-
-def count_volume(regions):
-    vol = 0
-    for r in regions:
-        v = (r[1]-r[0]+1)*(r[3]-r[2]+1)*(r[5]-r[4]+1)
-        vol += v
-    return vol
-
-regions = []
-with open('test2.txt', 'r') as f:
+with open('input1.txt', 'r') as f:
     for line in f:
         print(line.strip())
-        regions = process_line(line.strip(), regions)
-        print(f' actual number of "on" cubes: {count_volume(regions)}')
-        print(f' number of regions: {len(regions)}\n')
-print(f'final number of cubes in "on" state: {count_volume(regions)}')
+                
+        command, coords = line.split(' ')
+        coords = [int(l.split('=')[-1]) for x in coords.split(',') for l in x.split('..')]
+        if command == 'on':
+            for x, y, z in product(range(coords[0], coords[1]+1), range(coords[2], coords[3]+1),
+                                   range(coords[4], coords[5]+1)):                
+                l = f'{x}_{y}_{z}'
+                cubes.add(l)
+        elif command == 'off':
+               for x, y, z in product(range(coords[0], coords[1]+1), range(coords[2], coords[3]+1),
+                                   range(coords[4], coords[5]+1)):                
+                l = f'{x}_{y}_{z}'
+                cubes.discard(l)
+print(len(cubes))
--- 2021/day_22/problem1_wrong2.py
+++ 2021/day_22/problem1_wrong2.py
@@ -19,9 +19,36 @@ def check_overlap(regions):
     return False, None, None
 
 def get_limits(l):
-    sorted(l)
-    low = []
-    upper = []
+    l = sorted(l)
+    s = set(l)
+    if len(s) == 1:
+        lower = [l[0]]
+        upper = [l[0]]
+    elif len(s) == 2:
+        if l[1] != l[2]:
+            lower = [l[0]]
+            upper = [l[2]]
+        else:
+            if l[0]==l[1]:
+                lower = [l[0], l[0]+1]
+                upper = [l[0], l[2]]
+            else:
+                lower = [l[0], l[1]]
+                upper = [l[1]-1, l[1]]
+    elif len(s) == 3:
+        if l[0]==l[1]:
+            lower = [l[0], l[2]+1]
+            upper = [l[2], l[3]]
+        elif l[1]==l[2]:
+            lower = [l[0], l[1], l[2]+1]
+            upper = [l[1]-1, l[2], l[3]]
+        else:
+            lower = [l[0], l[1]]
+            upper = [l[1]-1, l[2]]
+    else:
+        lower = [l[0], l[1], l[2]+1]
+        upper = [l[1]-1, l[2], l[3]]
+    return lower, upper
 
 def chop_on(i1, i2, add_regions, remove_regions):
     r1 = add_regions[i1]
@@ -33,16 +60,11 @@ def chop_on(i1, i2, add_regions, remove_regions):
     elif contained(r2, r1):
         remove += [r2]
     else:
-        shift_min = [0,0,1]
-        shift_max = [-1,0,0]
-        x = sorted([r1[0], r1[1], r2[0], r2[1]])
-        y = sorted([r1[2], r1[3], r2[2], r2[3]])
-        z = sorted([r1[4], r1[5], r2[4], r2[5]])
-        print(x, y, z)
-        for ix, iy, iz in product(range(len(x)-1), range(len(y)-1), range(len(z)-1)):
-            r = [x[ix]+shift_min[ix], x[ix+1]+shift_max[ix],
-                 y[iy]+shift_min[iy], y[iy+1]+shift_max[iy],
-                 z[iz]+shift_min[iz], z[iz+1]+shift_max[iz]]
+        x_low, x_up = get_limits([r1[0], r1[1], r2[0], r2[1]])
+        y_low, y_up = get_limits([r1[2], r1[3], r2[2], r2[3]])
+        z_low, z_up = get_limits([r1[4], r1[5], r2[4], r2[5]])
+        for ix, iy, iz in product(range(len(x_low)), range(len(y_low)), range(len(z_low))):
+            r = [x_low[ix], x_up[ix], y_low[iy], y_up[iy], z_low[iz], z_up[iz]]
             if contained(r, r1) and contained(r, r2):
                 # print('*', r, '*', r1, contained(r, r1), ';', r2, contained(r, r2))
                 remove.append(r)
@@ -56,15 +78,11 @@ def chop_off(i1, off, add_regions, remove_regions):
     if contained(r1, off):
         remove.append(r2)
     else:
-        shift_min = [0,0,1]
-        shift_max = [-1,0,0]
-        x = sorted([r1[0], r1[1], r2[0], r2[1]])
-        y = sorted([r1[2], r1[3], r2[2], r2[3]])
-        z = sorted([r1[4], r1[5], r2[4], r2[5]])
-        for ix, iy, iz in product(range(len(x)-1), range(len(y)-1), range(len(z)-1)):
-            r = [x[ix]+shift_min[ix], x[ix+1]+shift_max[ix],
-                y[iy]+shift_min[iy], y[iy+1]+shift_max[iy],
-                z[iz]+shift_min[iz], z[iz+1]+shift_max[iz]]
+        x_low, x_up = get_limits([r1[0], r1[1], r2[0], r2[1]])
+        y_low, y_up = get_limits([r1[2], r1[3], r2[2], r2[3]])
+        z_low, z_up = get_limits([r1[4], r1[5], r2[4], r2[5]])
+        for ix, iy, iz in product(range(len(x_low)), range(len(y_low)), range(len(z_low))):
+            r = [x_low[ix], x_up[ix], y_low[iy], y_up[iy], z_low[iz], z_up[iz]]
             # print(r)
             if contained(r, r1) and contained(r, r2):
                 # print('*', r, '*', r1, contained(r, r1), ';', r2, contained(r, r2))
@@ -85,13 +103,24 @@ def process_line(line, add_regions, remove_regions):
     if command == 'on':
         add_regions.append(coords)
         i2 = len(add_regions)-1
+        add = []
+        remove = []
         for i1 in range(len(add_regions)-1):
             if overlap(add_regions[i1], add_regions[i2]):
                 # print(len(add_regions), i1, i2, add_regions[i1], add_regions[i2])
-                add, remove = chop_on(i1, i2, add_regions, remove_regions)
-                print(len(add), len(remove))
+                _, r = chop_on(i1, i2, add_regions, remove_regions)
+                if len(r)>1:
+                    print('CONFUSION!!!!')
+                remove += r
+                # print('removing', r)
+        for i1 in range(len(remove_regions)):
+            for i2 in range(len(remove)):
+                if overlap(remove_regions[i1], remove[i2]):
+                    _, r = chop_off(i1, remove[i2], remove_regions, [])
+                    # print('adding', r)
+                    add += r
+                # print(len(add), len(remove))
                 # add_regions += add
-                # remove_regions += remove
             # overlap, i1, i2 = check_overlap(regions)
     elif command == 'off':
         # print(command, coords)
@@ -111,8 +140,8 @@ def process_line(line, add_regions, remove_regions):
                     _, r = chop_on(i1, i2, remove, [])
                     # print('adding', r)
                     add += r
-        add_regions += add
-        remove_regions += remove_regions
+    add_regions += add
+    remove_regions += remove
         #     print(overlap, i1, regions[i1])
         #     regions = chop_off(i1, coords, regions)            
         #     overlap, i1, _ = check_overlap(regions+[coords])
@@ -132,12 +161,13 @@ def count_volume(add_regions, remove_regions):
 
 add_regions = []
 remove_regions = []
-with open('test2.txt', 'r') as f:
+with open('test1.txt', 'r') as f:
     for line in f:
         print(line.strip())
         add_regions, remove_regions = process_line(line.strip(), add_regions, remove_regions)
-        print(' +: ', add_regions)
-        print(' -: ', remove_regions)
+        # print(' +: ', add_regions)
+        # print(' -: ', remove_regions)
         print(f' actual number of "on" cubes: {count_volume(add_regions, remove_regions)}')
         # print(f' number of regions: {len(regions)}\n')
 # print(f'final number of cubes in "on" state: {count_volume(regions)}')
+
