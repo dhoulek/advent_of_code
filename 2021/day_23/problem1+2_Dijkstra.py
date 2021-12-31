c5ea75b725efbf34953f2ed23834e5e6be463cad
David Holec <david.holec@unileoben.ac.at>
2021-12-31 16:48:18 +0100

    day 23: corrected problem 2 + implemented Dijkstra algorithm

--- 2021/day_23/problem1+2_Dijkstra.py
+++ 2021/day_23/problem1+2_Dijkstra.py
@@ -15,20 +15,29 @@ def get_rooms(positions):
                               'c': 4,
                               'd': 6
                               }
-    levels = [list('.#.#.#.') for i in range(4)]
+    levels = [list('.#.#.#.') for i in range(int(len(names)/4))]
     for i, a in enumerate(positions):
         if re.match('[a-d]', a[0]):
             pos = positions_for_printing[a[0]]
             t = names[i]
+            # print(a, int(a[1:])-1)
             levels[int(a[1:])-1][pos] = t
     return levels
 
+def code_configuration(positions):
+    code = get_hallway(positions)
+    levels = get_rooms(positions)
+    for l in levels:
+        code += '|'
+        code += ''.join([l[i] for i in [0,2,4,6]])
+    return code
+    
 def print_configuration(positions):
     print('#############')
     print(f'#{get_hallway(positions)}#')
     levels = get_rooms(positions)
     print(f'###{"".join(levels[0])}###')
-    for i in range(1, 4):
+    for i in range(1, len(levels)):
         print(f'  #{"".join(levels[i])}#  ')
     print('  #########  ')
     
@@ -40,25 +49,85 @@ def is_finished(i, positions):
         for io, o in enumerate(positions):
             pos = o[0]
             level = int(o[1:])
+            # print(o, positions[i], io, i, names[io] != names[i], level, this_level)
             if names[io] != pos.upper() and pos == positions[i][0] and level > this_level:
                 finished = False
+    # print(finished)
     return finished
-    
-def get_next_moves(positions):
+
+positions_of_rooms = {'A': 2,
+                      'B': 4,
+                      'C': 6,
+                      'D': 8
+                    }
+energies = {'A': 1,
+            'B': 10,
+            'C': 100,
+            'D': 1000
+            }
+
+
+names = ['A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'C', 'C', 'C', 'C', 'D', 'D', 'D', 'D']
+
+positions = ['a4', 'd4', 'd2', 'c3',
+             'a1', 'c1', 'b3', 'c2',
+             'b1', 'c4', 'b2', 'd3',
+             'b4', 'd1', 'a2', 'a3']
+positions = ['a2', 'd2', 'a3', 'a4',
+             'a1', 'c1', 'b3', 'b4',
+             'b1', 'c2', 'c3', 'c4',
+             'b2', 'd1', 'd3', 'd4']
+target = ['a1', 'a2', 'a3', 'a4',
+          'b1', 'b2', 'b3', 'b4',
+          'c1', 'c2', 'c3', 'c4',
+          'd1', 'd2', 'd3', 'd4']
+
+### TEST PART 1
+# names = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D']
+# positions = ['a2', 'd2',
+#              'a1', 'c1',
+#              'b1', 'c2',
+#              'b2', 'd1']
+# target = ['a1', 'a2',
+#           'b1', 'b2',
+#           'c1', 'c2',
+#           'd1', 'd2']
+
+### EASY TEST
+# names = ['A', 'B', 'C', 'D']
+# positions = ['d1',
+#              'b1',
+#              'c1',
+#              'a1']
+# target = ['a1',
+#           'b1',
+#           'c1',
+#           'd1',]
+print_configuration(positions)
+# print(code_configuration(positions))
+
+graph = {code_configuration(positions): [None, 0]}
+explored = []
+moves = [positions]
+done = False
+MAX_DEPTH = int(len(positions)/4)
+
+while not done:
+# for _ in range(10):
+    # print(code_configuration(target) in graph.keys())
+    # print(len(graph), len(moves))
+    dist = [graph[code_configuration(m)][1] for m in moves]
+    next_index = dist.index(min(dist))
+    positions = moves.pop(next_index)
+    # print(dist, next_index)
+    explored.append(code_configuration(positions))
+    print(dist[next_index], len(explored), len(moves))
+    # print(code_configuration(positions), positions)
+    this_node = code_configuration(positions)
     hallway = list(get_hallway(positions))
-    positions_of_rooms = {'A': 2,
-                          'B': 4,
-                          'C': 6,
-                          'D': 8
-                          }
-    energies = {'A': 1,
-                'B': 10,
-                'C': 100,
-                'D': 1000
-                }
-    min_energy = int(1e10)
     done = True
     for i in range(len(positions)):
+        # print(i, positions[i], is_finished(i, positions))
         if not is_finished(i, positions):
             done = False
             pos = int(positions[i][1:])
@@ -82,13 +151,21 @@ def get_next_moves(positions):
                                 # already one correct in
                                 occupied += 1
                 if free and room_ready:
-                    level = 4-occupied
+                    level = MAX_DEPTH-occupied
                     next = f'{a.lower()}{level}'
                     energy = energies[a]*(dist+level)
                     # print(pos, a, positions[i], next, energy, free, room_ready)
                     next_pos = positions.copy()
                     next_pos[i] = next
-                    min_energy = min(min_energy, get_next_moves(next_pos)+energy)
+                    code = code_configuration(next_pos)
+                    if code in graph.keys():
+                        if graph[code][1] > graph[this_node][1]+energy:
+                            graph[code] = [this_node, graph[this_node][1]+energy]
+                    else:
+                        graph[code] = [this_node, graph[this_node][1]+energy]
+                    # if code not in explored:
+                    if next_pos not in moves:
+                        moves.append(next_pos)
             else:
                 # amphipod in wrong room
                 level = int(positions[i][1:])
@@ -115,29 +192,14 @@ def get_next_moves(positions):
                                 # print(pos, a, positions[i], next, energy, level, dist, free, free_hallway)
                                 next_pos = positions.copy()
                                 next_pos[i] = next
-                                min_energy = min(min_energy, get_next_moves(next_pos)+energy)
-    if done:
-        return 0
-    else:
-        return min_energy
+                                code = code_configuration(next_pos)
+                                if code in graph.keys():
+                                    if graph[code][1] > graph[this_node][1]+energy:
+                                        graph[code] = [this_node, graph[this_node][1]+energy]
+                                else:
+                                    graph[code] = [this_node, graph[this_node][1]+energy]
+                                # if code not in explored:
+                                if next_pos not in moves:
+                                    moves.append(next_pos)
 
-names = ['A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'C', 'C', 'C', 'C', 'D', 'D', 'D', 'D']
-
-print('test case:')
-positions = ['a4', 'd4', 'd2', 'c3',
-             'a1', 'c1', 'b3', 'c2',
-             'b1', 'c4', 'b2', 'd3',
-             'b4', 'd1', 'a2', 'a3']
-print_configuration(positions)
-# min_energy = get_next_moves(positions)
-# print(f'minimum energy is: {min_energy}')
-print('')
-
-print('puzzle input:')
-positions = ['a1', 'd1', 'd2', 'c3',
-             'c4', 'd4', 'b3', 'c2',
-             'a4', 'c1', 'b2', 'd3',
-             'b4', 'b1', 'a2', 'a3']
-print_configuration(positions)
-# min_energy = get_next_moves(positions)
-# print(f'minimum energy is: {min_energy}')
+print(f'minumum energy: {graph[code_configuration(target)][1]}')
