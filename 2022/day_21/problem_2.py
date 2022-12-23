1f1a0565dbf1796e7cd97f6a0a18ee1c0e742251
David Holec <david.holec@unileoben.ac.at>
2022-12-23 21:08:00 +0100

    day 21, problem 2

--- 2022/day_21/problem_2.py
+++ 2022/day_21/problem_2.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 monkeys = {}
 with open('input.txt', 'r') as f:
     for line in f.readlines():
@@ -6,93 +8,74 @@ with open('input.txt', 'r') as f:
         words[1] = words[1].strip().split()
         if len(words[1]) == 1:
             # shouting number
-            monkeys[monkey] = int(words[1][0])
+            monkeys[monkey] = {'A': None, 'B': None, 'op': None, 'value': int(words[1][0])}
         else:
             # shouting operation
-            monkeys[monkey] = words[1]
+            monkeys[monkey] = {'A': words[1][0], 'B': words[1][2], 'op': words[1][1],
+                               'value': None}
+# ignore 'humn'
+monkeys['humn']['value'] = None
 
+# fill in all numbers shouted out
+def fill_numbers():
+    for m in tqdm(monkeys, desc='filling numbers'):
+        for i in ['A', 'B']:
+            if type(monkeys[m][i]) == str and monkeys[monkeys[m][i]]['value'] != None:
+                monkeys[m][i] = monkeys[monkeys[m][i]]['value']
+                
+def perform_operations():
+    for m in tqdm(monkeys, desc='performing operations'):
+        numbers = [type(monkeys[m][X]) != str and monkeys[m][X] != None for X in ['A', 'B', 'value']]
+        if sum(numbers) == 2:
+            # two numbers known -> perform operations
+            if numbers == [True, True, False]:
+                # print(monkeys[m])
+                if monkeys[m]['op'] == '+':
+                    monkeys[m]['value'] = monkeys[m]['A'] + monkeys[m]['B']
+                elif monkeys[m]['op'] == '-':
+                    monkeys[m]['value'] = monkeys[m]['A'] - monkeys[m]['B']
+                elif monkeys[m]['op'] == '*':
+                    monkeys[m]['value'] = monkeys[m]['A'] * monkeys[m]['B']
+                elif monkeys[m]['op'] == '/':
+                    monkeys[m]['value'] = monkeys[m]['A'] / monkeys[m]['B']
+            elif numbers == [True, False, True]:
+                # print(monkeys[m])
+                if monkeys[m]['op'] == '+':
+                    monkeys[monkeys[m]['B']]['value'] =  monkeys[m]['value'] - monkeys[m]['A']
+                elif monkeys[m]['op'] == '-':
+                    monkeys[monkeys[m]['B']]['value'] = monkeys[m]['A'] - monkeys[m]['value']
+                elif monkeys[m]['op'] == '*':
+                    monkeys[monkeys[m]['B']]['value'] = monkeys[m]['value'] / monkeys[m]['A']
+                elif monkeys[m]['op'] == '/':
+                    monkeys[monkeys[m]['B']]['value'] = monkeys[m]['A'] / monkeys[m]['value']
+            elif numbers == [False, True, True]:
+                # print(monkeys[m])
+                if monkeys[m]['op'] == '+':
+                    monkeys[monkeys[m]['A']]['value'] =  monkeys[m]['value'] - monkeys[m]['B']
+                elif monkeys[m]['op'] == '-':
+                    monkeys[monkeys[m]['A']]['value'] = monkeys[m]['B'] + monkeys[m]['value']
+                elif monkeys[m]['op'] == '*':
+                    monkeys[monkeys[m]['A']]['value'] = monkeys[m]['value'] / monkeys[m]['B']
+                elif monkeys[m]['op'] == '/':
+                    monkeys[monkeys[m]['A']]['value'] = monkeys[m]['B'] * monkeys[m]['value']
+            
 
-# part 1: do until root has 1 number
-M1 = monkeys['root'][0]
-M2 = monkeys['root'][2]
-while type(monkeys[M1]) == list and type(monkeys[M2]) == list:
-    op_monkeys = 0
-    for monkey in monkeys:
-        # we will ignore 'human'
-        if monkey != 'humn':            
-            if type(monkeys[monkey]) == list:
-                m1 = monkeys[monkey][0]
-                A = monkeys[m1]
-                m2 = monkeys[monkey][2]
-                B = monkeys[m2]
-                # print(monkey, type(A), type(B))
-                if type(A) != list and type(B) != list and m1 != 'humn' and m2 != 'humn':
-                    # inputs ready!
-                    if monkeys[monkey][1] == '+':
-                        monkeys[monkey] = A + B
-                    elif monkeys[monkey][1] == '-':
-                        monkeys[monkey] = A - B
-                    elif monkeys[monkey][1] == '*':
-                        monkeys[monkey] = A * B
-                    elif monkeys[monkey][1] == '/':
-                        monkeys[monkey] = A / B
-                else:
-                    # still operation monkey
-                    op_monkeys += 1
-        print(f'numbers: {len(monkeys)-op_monkeys};   operations: {op_monkeys}')
+# part 1 - getting to the 'root' operation
+while type(monkeys['root']['A']) == str and type(monkeys['root']['B']) == str:
+    fill_numbers()
+    perform_operations()
+
+# part 2 - perform 'root', i.e. '=' opoeration
+if type(monkeys['root']['A']) == str:
+    monkeys[monkeys['root']['A']]['value'] = monkeys['root']['B']
+else:
+    monkeys[monkeys['root']['B']]['value'] = monkeys['root']['A']
+# print(monkeys['root'])
 # print(monkeys)
 
-# fill in all known numbers
-for monkey in monkeys:
-    if type(monkeys[monkey]) == list:
-        for i in [0, 2]:
-            m = monkeys[monkey][i]
-            if type(monkeys[m]) != list and m != 'humn':
-                monkeys[monkey][i] = monkeys[m]
-# print(monkeys)                
-
-
-def inv_op(A, B, res, op):
-    print(A, B, res, op)
-    if type(A) == str:
-        # solving A
-        if op == '+':
-            return res - B
-        elif op == '-':
-            return res + B
-        elif op == '*':
-            return res / B
-        elif op == '/':
-            return res * B
-    else:
-        # solving B
-        if op == '+':
-            return res - A
-        elif op == '-':
-            return A - res
-        elif op == '*':
-            return res / A
-        elif op == '/':
-            return A / res
-        
-# apply root operation
-if type(monkeys[M1]) == list:
-    res = monkeys[M2]
-    solving = M1
-else:
-    res = monkeys[M1]
-    solving = M2
-    
-# part 2: propagate, until we resolve 'humn'
-while solving != 'humn':
-    [A, op, B] = monkeys[solving]
-    if type(A) == str:
-        new_solving = A
-        new_res = inv_op(A, B, res, op)
-        print(f'{solving}: {A} {op} {B} = {res}  ==> {new_res} {op} {B} = {res}  ==> {A} = {new_res}')
-    else:
-        new_solving = B
-        new_res = inv_op(A, B, res, op)
-        print(f'{solving}: {A} {op} {B} = {res}  ==> {A} {op} {new_res} = {res}  ==> {B} = {new_res}')
-    res = new_res
-    solving = new_solving
+# part 3 - resolve 'humn' value
+while monkeys['humn']['value'] == None:
+    fill_numbers()
+    perform_operations()
+print()
+print(monkeys['humn'])lving
