efac89bcf3391421dfa8273cc8fc01e92c608612
David Holec <david.holec@unileoben.ac.at>
2022-12-24 10:21:28 +0100

    day 22, problem 1

--- 2022/day_22/problem_1.py
+++ 2022/day_22/problem_1.py
@@ -1,8 +1,19 @@
 import numpy as np
 
-inp = 'test'
+inp = 'input'
+path  =[]
 with open(f'{inp}.path') as f:
-    path = f.readline().strip()
+    n =''
+    for c in f.readline().strip():
+        if c.isnumeric():
+            n += c
+        else:
+            path += [int(n), c]
+            n = ''
+    if n != '':
+        path += [int(n)]
+print(path)
+    
 # get max line length
 max_length = 0
 inp_data = []
@@ -40,5 +51,55 @@ for j in range(board.shape[1]):
     while board[bottom, j] == 0:
         bottom -= 1
     columns.append((top, bottom))
+
+print(board.shape)
+print(rows)
+print(columns)
+
+def read(pos):
+    return board[tuple(pos)]
+
+def new(pos, dir):
+    new_pos = np.array(pos) + moves[dir]
+    # print(f'  . new position: {new_pos}')
+    if dir == 0:
+        # horizontal move, check range
+        if new_pos[1] > rows[pos[0]][1]:
+            new_pos[1] = rows[pos[0]][0]
+    elif dir == 2:
+        # horizontal move, check range
+        if new_pos[1] < rows[pos[0]][0]:
+            new_pos[1] = rows[pos[0]][1]
+    elif dir == 1:
+        # vertical move, check range
+        if new_pos[0] > columns[pos[1]][1]:
+            new_pos[0] = columns[pos[1]][0]
+    elif dir == 3:
+        # vertical move, check range
+        if new_pos[0] < columns[pos[1]][0]:
+            new_pos[0] = columns[pos[1]][1]
+    # print(f'  + wrapped new position: {new_pos}')
+    return new_pos
     
-print(rows, columns)
+
+moves = [(0,1), (1,0), (0,-1), (-1,0)]
+pos = np.array([0, rows[0][0]])
+dir = 0
+print(f'starting position: {pos} in direction {moves[dir]}')
+
+for i in path:
+    if i == 'L':
+        dir = (dir-1) % 4
+    elif i == 'R':
+        dir = (dir+1) % 4
+    else:
+        for _ in range(i):
+            new_pos = new(pos, dir)
+            if read(new_pos) == 1:
+                pos = new_pos
+            else:
+                break
+    print(f'after {i} at position: {pos} in direction {moves[dir]}')
+    
+print(f'final score: {1000*(pos[0]+1)+4*(pos[1]+1)+dir}')
+
