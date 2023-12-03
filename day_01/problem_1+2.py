63eabd22b86bbdeabad0d8761c68310ffd39598d
David Holec <david.holec@unileoben.ac.at>
2023-12-03 22:33:10 +0100

    AoC 2023, day 1, added comments

--- day_01/problem_1+2.py
+++ day_01/problem_1+2.py
@@ -1,42 +1,58 @@
+# Function to replace words in a line with their corresponding numeric representations
 def replace_words(line):
+    # Dictionary mapping words to their numeric representations
+    # we keep the first and last letter to correctly decode 
+    # e.g. 'oneight' into '18'
     nums = dict(one='o1e', two='t2o', three='t3e', four='f4r', 
                 five='f5e', six='s6x', seven='s7n', eight='e8t', 
                 nine='n9e')
+    
+    # Replace words in the line with their numeric representations
     for k in nums.keys():
         line = line.replace(k, str(nums[k]))
     return line
 
+
+# Function to find the calibration value from a line
 def find_calibration_value(line):
-    # get list of characters
+    # Convert the line into a list of characters
     line = list(line.strip())
-    # search for first digit
+    
+    # Search for the first digit from the end of the line
     l = list(reversed(line.copy()))
     first = l.pop()
     while not str.isnumeric(first):
         first = l.pop()
-    # search for the last digit
+    
+    # Search for the last digit
     l = line.copy()
     last = l.pop()
     while not str.isnumeric(last):
         last = l.pop()
-    # return the number
-    return int(first+last)
+    
+    # Return the numeric value formed by concatenating the first and last digits
+    return int(first + last)
 
+
+# Lists to store calibration values for problem 1 and problem 2
 numbers = []
 numbers2 = []
 
+# Read input from a file ('input.txt') containing lines with words to be replaced
+# Change the file name to 'test.txt' or 'test2.txt' if needed for testing
 with open('input.txt', 'r') as f:
-# with open('test.txt', 'r') as f:
-# with open('test2.txt', 'r') as f:
+    # Iterate over each line in the file
     for line in f.readlines():
+        # Find and store the calibration value for the original line
         numbers.append(find_calibration_value(line))
+        
+        # Find and store the calibration value for the line with replaced words
         numbers2.append(find_calibration_value(replace_words(line)))
-# print(numbers2)
 
-# problem 1
+# Print results for problem 1
 print('Part 1:')
 print(f'Sum of all decoded calibration values is: {sum(numbers)}\n')
 
-# problem 2
+# Print results for problem 2
 print('Part 2:')
-print(f'Sum of all decoded calibration values is: {sum(numbers2)}\n')
+print(f'Sum of all decoded calibration values with replaced words is: {sum(numbers2)}')
