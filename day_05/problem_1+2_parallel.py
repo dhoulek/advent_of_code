70bb985424f74089886600afa9714dcdd40d528c
David Holec <david.holec@unileoben.ac.at>
2023-12-06 07:31:56 +0100

    AoC 2023, day 5, with quick and elegant solution

--- day_05/problem_1+2_parallel.py
+++ day_05/problem_1+2_parallel.py
@@ -1,11 +1,10 @@
-from tqdm import tqdm
+# Brute force solution for propagating seed by seed. Took about 1 hrs on mul-hpc-8.1, 6 core node
+
 from multiprocessing import Pool
 mappings = []
-max_affected = 0
 
 with open('input.txt', 'r') as f:
-# with open('test.txt', 'r') as 
-# f:
+# with open('test.txt', 'r') as f:
     # reading seeds
     seeds = [int(i) for i in f.readline().strip().split(':')[-1].split()]
     
@@ -26,14 +25,13 @@ with open('input.txt', 'r') as f:
             dest, src, length = [int(i) for i in l.strip().split()]
             mapping.append(dict(src_first=src, src_last=src+length-1, 
                                 dest_first=dest, dest_last=dest+length-1))
-            # max_affected = max([max_affected, src+length-1, dest+length-1])
     # add last mapping
     mappings.append(mapping)
-            
-# print(seeds)
-# print(mappings)
-# print(max_affected)
 
+# Part 1
+print('Part 1:')
+
+# List to store final locations after applying mappings            
 final_locations = []
 for seed in seeds:
     recipe = [seed]
@@ -47,23 +45,19 @@ for seed in seeds:
     # print(' -> '.join([str(s) for s in recipe]))
     final_locations.append(recipe[-1])
     
-print('Part 1:')
 print(f'The lowest location number is {min(final_locations)}\n')
 
-# print(sum([s for s in seeds[1::2]])/150000/3600)
-# print(sum([s for s in seeds[1::2]]))
-# all_seeds = []
-# for ini, length in zip(seeds[::2], seeds[1::2]):
-#     print(ini>max_affected, ini+length>max_affected)
-    # all_seeds += list(range(ini, ini+length))
-    
-# print(all_seeds)
 
+
+# Part 2
+print('Part 2:')
+
+# Function to apply mappings to a seed
 def propagate(seed):
     for mapping in mappings:
         for m in mapping:
             if m['src_first'] <= seed <= m['src_last']:
-                # found applicable map
+                # Found applicable map
                 seed += m['dest_first'] - m['src_first']
                 break
     return seed
@@ -72,12 +66,14 @@ lowest_location = int(1e10)
 i = 0
 for ini, length in zip(seeds[::2], seeds[1::2]):
     i += 1
-    print(f'Processing range {i}/{len(seeds)//2}, of length {length}')
+    print(f' Processing range {i}/{len(seeds)//2}, of length {length}')
+    
+    # Use multiprocessing to apply mappings to a range of seeds
     with Pool() as pool:
-      mapped_range = pool.map(propagate, range(ini, ini+length))
+        mapped_range = pool.map(propagate, range(ini, ini+length))
+        
     lowest_location = min(lowest_location, min(mapped_range))
     if lowest_location == 0:
         break
     
-print('Part 2:')
 print(f'The lowest location number is {lowest_location}')
